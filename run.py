3a5109d568e13a8aa0e64d38134cf24c45c6fddc
Eba Teshale <ebateshale40@gmail.com>
2026-04-29 12:55:42 +0300

    some fixed hings

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ from app.routes.auth import auth_bp
 from app.routes.main import main_bp
 from dotenv import load_dotenv
 import os
-
+from flask_cors import CORS
 # Load environment variables from .env
 load_dotenv()
 
@@ -14,9 +14,18 @@ app = Flask(
     template_folder='app/templates',
     static_folder='app/static'
 )
+
+CORS(
+    app,
+    supports_credentials=True,
+    origins=[
+        "https://ethio-frontend.vercel.app"
+    ]
+)
+
+app.secret_key = os.environ.get("SECRET_KEY")
 # Secret key for session management
 # app.config["SECRET_KEY"] = os.environ.get("feiwkabgwjbfsjfbsaifkbsfjhcsabfikcasfbcjsdfhbciaskfhecdapadaddfndsfehfh348fbdsfbsdkfbcs", "supersecretkey")
-app.secret_key = os.environ.get("SECRET_KEY")
 
 # Initialize OAuth with app
 oauth.init_app(app)
