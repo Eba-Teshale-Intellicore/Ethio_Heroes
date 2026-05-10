e2553bcf80b1d789c5ce3bd58c446edcb124263e
Eba Teshale <ebateshale40@gmail.com>
2026-05-10 14:10:10 +0300

    Fix duplicate Flask endpoint

--- app/routes/main.py
+++ app/routes/main.py
@@ -49,7 +49,7 @@ def get_heroes():
 
     # ------------------- Director Page -------------------
 @main_bp.route("/api/director")
-def director_heroes():
+def get_heroes():
 
     conn = get_db()
     with conn.cursor() as cur:
